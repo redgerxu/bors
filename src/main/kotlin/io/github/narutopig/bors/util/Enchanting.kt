0d41f008c6053f510dadbc9ed1ca5fccaab9d444
pogtato <69479556+narutopig@users.noreply.github.com>
2021-01-23 19:04:39 -0800

    cleanup

--- src/main/kotlin/io/github/narutopig/bors/util/Enchanting.kt
+++ src/main/kotlin/io/github/narutopig/bors/util/Enchanting.kt
@@ -38,7 +38,7 @@ object Enchanting {
             }
     }
 
-    fun hasEnchant(item: ItemStack, enchantment: Enchantment): Boolean {
+    private fun hasEnchant(item: ItemStack, enchantment: Enchantment): Boolean {
         return item.enchantments[enchantment] != null
     }
 
@@ -52,31 +52,6 @@ object Enchanting {
 
     fun canEnchant(item: ItemStack?, enchantment: Enchantment): Boolean {
         return enchantment.canEnchantItem(item!!) && enchantment.itemTarget.includes(item)
-    }
-
-    fun addEnchant(item: ItemStack, enchantment: Enchantment, level: Int) {
-        if (!canEnchant(item, enchantment)) return
-        val itemMeta = item.itemMeta ?: return
-        var lore = itemMeta.lore
-        if (lore == null) lore = ArrayList()
-        if (hasEnchant(item, enchantment)) {
-            var i = 0
-            for (line in lore) {
-                if (ChatColor.stripColor(line)!!.split(" ").toTypedArray()[0] == enchantment.name) {
-                    val newLine = EnchantmentWrapper.color.toString() + enchantment.name + " " + General.toRoman(level)
-                    lore[i] = newLine
-                    break
-                }
-                i++
-            }
-        } else {
-            val newLine = EnchantmentWrapper.color.toString() + enchantment.name + " " + General.toRoman(level)
-            lore.add(newLine)
-        }
-        itemMeta.lore = lore
-        item.itemMeta = itemMeta
-        item.removeEnchantment(enchantment)
-        item.addEnchantment(enchantment, level)
     }
 
     fun addEnchant(itemStack: ItemStack, enchantment: EnchantmentWrapper, level: Int): ItemStack? {
