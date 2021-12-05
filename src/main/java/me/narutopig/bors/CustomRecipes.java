48f841753f8eb49e844cfcd1eead0096291e9210
pogtato <69479556+narutopig@users.noreply.github.com>
2021-12-04 21:36:34 -0800

    Refractoring

--- src/main/java/me/narutopig/bors/CustomRecipes.java
+++ src/main/java/me/narutopig/bors/CustomRecipes.java
@@ -1,13 +1,22 @@
-package me.narutopig.bors.recipes;
+package me.narutopig.bors;
 
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomRecipes {
-    public static Recipe tridentRecipe() {
+    public static final ShapedRecipe trident = tridentRecipe();
+    public static final List<ShapedRecipe> recipes = new ArrayList<>();
+
+    static {
+        recipes.add(trident);
+    }
+
+    public static ShapedRecipe tridentRecipe() {
         ItemStack trident = new ItemStack(Material.TRIDENT);
         NamespacedKey key = NamespacedKey.minecraft("trident");
 
--- src/main/java/me/narutopig/bors/Main.java
+++ src/main/java/me/narutopig/bors/Main.java
@@ -6,9 +6,9 @@ import me.narutopig.bors.enchantments.enchants.TelekinesisEnchant;
 import me.narutopig.bors.lifesteal.commands.SetHealth;
 import me.narutopig.bors.lifesteal.listeners.Lifesteal;
 import me.narutopig.bors.listeners.AntiCombatLog;
-import me.narutopig.bors.recipes.CustomRecipes;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
@@ -22,10 +22,16 @@ public class Main extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(new AntiCombatLog(), this);
         getCommand("borsenchant").setExecutor(new EnchantCommand());
         getCommand("sethealth").setExecutor(new SetHealth());
-        Bukkit.addRecipe(CustomRecipes.tridentRecipe());
+        addRecipes();
     }
 
     @Override
     public void onDisable() {
+    }
+
+    public static void addRecipes() {
+        for (Recipe recipe : CustomRecipes.recipes) {
+            Bukkit.addRecipe(recipe);
+        }
     }
 }
