64dc9b9d45d1967975ddb685a3901ed79db0824d
pogtato <69479556+narutopig@users.noreply.github.com>
2021-12-29 20:16:08 -0800

    removed kill cooldown

--- src/main/kotlin/io/github/narutopig/bors/listeners/Lifesteal.kt
+++ src/main/kotlin/io/github/narutopig/bors/listeners/Lifesteal.kt
@@ -13,9 +13,6 @@ import java.util.*
 
 
 class Lifesteal : Listener {
-    private val killManager: MutableMap<UUID, Long> = HashMap() // keeps tracked of how long ago you were killed
-
-
     companion object {
         fun setMaxHealth(player: Player, hearts: Double) {
             player.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!.baseValue = hearts
@@ -46,13 +43,7 @@ class Lifesteal : Listener {
     fun onPlayerDeath(event: PlayerDeathEvent) {
         val killed = event.entity
         val killer = killed.killer ?: return
-        val uuid = killed.uniqueId
-        val currTime = System.currentTimeMillis()
-        if (currTime - killManager.getOrDefault(uuid, 0L) >= 15L * 1000) {
-            // runs only if the last time the victim was killed was more than 15 seconds ago
-            setMaxHealth(killed, killed.maxHealth - 2)
-            setMaxHealth(killed, killer.maxHealth + 2)
-        }
-        killManager[uuid] = currTime
+        setMaxHealth(killed, killed.maxHealth - 2)
+        setMaxHealth(killed, killer.maxHealth + 2)
     }
 }
